8038b94a0ef9717409947c82b1928a3e8e8fbc20
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-06-14 13:37:16 +0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
 import * as React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="mx-auto text-center flex h-screen">
+      <div className="m-auto pb-20">
+        <h1 className="font-bold text-8xl">404</h1>
+        <h1 className="text-3xl mb-20 font-semibold">Oops! Ga ketemu :(</h1>
+        <Link to="/" className="shadow p-3 m-2 bg-royah-yellow rounded-xl">
+          halaman awal
+        </Link>
+        <Link
+          to="/random"
+          className="shadow p-3 m-2 bg-royah-jellyfish rounded-xl"
+        >
+          random profile
+        </Link>
+      </div>
+    </div>
   </Layout>
 )
 
