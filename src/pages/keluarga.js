423e5a420eaca5bb9e947df45527e0e7cab9018c
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-06-19 23:29:52 +0700

    implement search feature

--- src/pages/keluarga.js
+++ src/pages/keluarga.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageHeader from "../components/pageheader"
 import { FaFilter } from "@react-icons/all-files/fa/FaFilter"
+import { debounce, throttle } from "throttle-debounce"
 
 const Keluarga = () => {
   const data = useStaticQuery(graphql`
@@ -31,6 +32,7 @@ const Keluarga = () => {
     }
   `)
   const nodeList = data.allPeopleCsv.edges
+  const [search, setSearch] = React.useState("")
 
   return (
     <Layout>
@@ -41,6 +43,12 @@ const Keluarga = () => {
           type="text"
           placeholder="cari berdasarkan nama atau absen"
           className="px-4 text-gray-500 py-2 w-full mb-8 shadow rounded-lg focus:outline-none focus:ring-2 focus:border-transparent"
+          onChange={e =>
+            debounce(
+              500,
+              throttle(500, setSearch(e.target.value.toLowerCase()))
+            )
+          }
         />
         <button className="px-4 text-gray-500 py-2 ml-2 text-center mb-8 shadow rounded-lg focus:outline-none focus:ring-2 focus:border-transparent">
           <FaFilter />
@@ -48,28 +56,35 @@ const Keluarga = () => {
       </div>
       <div className="mx-auto w-11/12 lg:w-10/12">
         <div className="flex flex-wrap justify-center lg:px-5 mx-auto">
-          {nodeList.map(({ node }) => {
-            return (
-              <div
-                className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 flex flex-wrap justify-center my-6 p-2 rounded-xl"
-                key={node.absen}
-              >
-                <Link to={`/${node.fields.slug}`} className="text-center">
-                  <div className="card-zoom mx-auto rounded-3xl shadow">
-                    <div
-                      className="card-zoom-image rounded-3xl mx-auto"
-                      style={{
-                        backgroundImage: `url(${node.photo.childImageSharp.fixed.src})`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="w-32 mx-auto mt-2">
-                    {node.nickname ? node.nickname : node.name}
-                  </div>
-                </Link>
-              </div>
+          {nodeList
+            .filter(
+              ({ node }) =>
+                node.name.toLowerCase().includes(search) ||
+                node.nickname.toLowerCase().includes(search) ||
+                node.absen.toLowerCase().includes(search)
             )
-          })}
+            .map(({ node }) => {
+              return (
+                <div
+                  className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 flex flex-wrap justify-center my-6 p-2 rounded-xl"
+                  key={node.absen}
+                >
+                  <Link to={`/${node.fields.slug}`} className="text-center">
+                    <div className="card-zoom mx-auto rounded-3xl shadow">
+                      <div
+                        className="card-zoom-image rounded-3xl mx-auto"
+                        style={{
+                          backgroundImage: `url(${node.photo.childImageSharp.fixed.src})`,
+                        }}
+                      ></div>
+                    </div>
+                    <div className="w-32 mx-auto mt-2">
+                      {node.nickname ? node.nickname : node.name}
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
         </div>
       </div>
     </Layout>
