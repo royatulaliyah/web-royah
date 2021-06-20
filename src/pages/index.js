b8bd241e3aaa4f2ad421d689e8fc6ed9c3508bb1
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-06-20 20:17:15 +0700

    use gatsby image at index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,22 @@ import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import illustration from "../images/teamwork.png"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Coming Soon" />
     <div className="flex min-h-screen z-10">
       <div className="m-auto">
-        <img
+        <StaticImage
+          src="../images/teamwork.png"
+          className="mx-auto"
           draggable="false"
-          src={illustration}
-          alt="teamwork"
-          className="mx-auto max-h-72"
+          alt="under construction"
+          layout="fixed"
+          height="200"
         />
+
         <h1 className="text-4xl mt-4 mb-10 text-center">coming soon!</h1>
       </div>
     </div>
